3e06e68bbf7acc4f6a6f4dac179c79eb03586e88
moaalaa <moaalaa16@gmail.com>
2021-02-10 14:43:33 +0200

    Adding Laravel Support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.1] - 2021-02-10
+- Adding Laravel Support
+
 ## [1.1.0] - 2021-01-08
 - Using Jokes Api
 
---
+++ src/JokesServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MoaAlaa\SayAJoke;
+
+use Illuminate\Support\ServiceProvider;
+
+class JokesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
